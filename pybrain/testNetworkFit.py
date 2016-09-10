47ff73cf6744fabb2dc157f8180edb4afbf411cd
Terry Ott <Terry.Ott@gmail.com>
2016-09-10 17:43:44 +0000

    Updated testfit, found we busted input vectors, rewinding to restart training

--- pybrain/testNetworkFit.py
+++ pybrain/testNetworkFit.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-import logging
 import json
-import pybrain
 import pybrain.tools.shortcuts
 import pybrain.tools.customxml.networkreader
+import logging
 import argparse
+import datetime
+import pprint
 
 
 def main():
@@ -30,7 +31,7 @@ def readActivities(activityFile):
     with open(activityFile, 'r') as activitiesJson:
         activities = json.load(activitiesJson)
 
-    logging.info("Read activities from {0}".format(activityFile) )
+    print("Read activities from {0}".format(activityFile) )
 
     return activities
 
@@ -40,15 +41,102 @@ def createNetworkFromFile(neuralNetXmlFile):
     neuralNet = pybrain.tools.shortcuts.buildNetwork(7, 4, 1, bias=True)
     pybrain.tools.customxml.networkreader.NetworkReader.readFrom(neuralNetXmlFile)
 
-    logging.info("Read neural net from {0}".format(neuralNetXmlFile) )
+    print("Read neural net from {0}".format(neuralNetXmlFile) )
 
     return neuralNet
 
 
 def testFit(activities, neuralNet):
-    pass
+    timestamps = sorted( activities.keys() )
+
+    stats = {
+        'datapoints': 0,
+        'totalError': 0.0
+    }
+
+    for currTimestamp in timestamps:
+        entryTimestamp = datetime.datetime.strptime(currTimestamp, "%Y%m%d %H%M%S")
+        # print( "Entry timestamp: {0}".format(entryTimestamp) )
+
+        for currActivity in activities[currTimestamp]:
+            # Is it a button press?
+            if currActivity['activity_type'] == "Request Elevator":
+                print( "Button press at {0} on floor index {1}".format(
+                    entryTimestamp, currActivity['start_floor']) )
+
+                neuralNetResult = activateNet(neuralNet, entryTimestamp)
+
+                print( "Neural net result: {0:5.3f}".format(
+                   neuralNetResult) )
+
+                break
+
+            break
+
+        break
+
+
+def activateNet(neuralNet, entryTimestamp):
+
+    inputVector =  convertDatetimeToNormalizedInputVector(entryTimestamp)
+    #pprint.pprint(inputVector)
+    #return None
+    return neuralNet.activate( inputVector )[0]
+
+
+
+def convertDatetimeToNormalizedInputVector(entryTimestamp):
+
+    # There are seven values in the input vector
+    #
+    #   Year        (normalized from 1970-2069)
+    #   Month       (normalized from 1-12)
+    #   Day         (normalized from 1-31)
+    #   Day of week (normalized from 1-7)
+    #   Hour        (normalized from 0-23)
+    #   Minute      (normalized from 0-59)
+    #   Second      (normalized from 0-59)
+    return [
+        normalizeYear(entryTimestamp.year),
+        normalizeMonth(entryTimestamp.month),
+        normalizeDay(entryTimestamp.day),
+        normalizeWeekday(entryTimestamp.date().weekday()),
+        normalizeHour(entryTimestamp.hour),
+        normalizeMinute(entryTimestamp.minute),
+        normalizeSecond(entryTimestamp.second)
+    ]
+
+
+def normalizeYear(year):
+    return (year - 1970) / 100.0
+
+
+def normalizeMonth(month):
+    return (month - 1) / 1.0
+
+
+def normalizeDay(day):
+    return (day - 1) / 31.0
+
+
+def normalizeWeekday(weekday):
+    return weekday / 6.0
+
+
+def normalizeHour(hour):
+    return hour / 23.0
+
+
+def normalizeMinute(minute):
+    return minute / 59.0
+
+
+def normalizeSecond(second):
+    return second / 59.0
+
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     main()
+
